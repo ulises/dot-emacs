a97f36cb6eb7196e3d73b920b848dfedc900c9e3
Ulises Cervino Beresi <ulises@cloudant.com>
2014-02-05 14:23:59 +0000

    Configure git-gutter+ keybindings.

--- init.el
+++ init.el
@@ -320,6 +320,21 @@
 ;; git-gutter
 (global-git-gutter+-mode t)
 
+(eval-after-load 'git-gutter+
+  '(progn
+     ;;; Jump between hunks
+     (define-key git-gutter+-mode-map (kbd "C-x n") 'git-gutter+-next-hunk)
+     (define-key git-gutter+-mode-map (kbd "C-x p") 'git-gutter+-previous-hunk)
+
+     ;;; Act on hunks
+     (define-key git-gutter+-mode-map (kbd "C-x v =") 'git-gutter+-show-hunk)
+     (define-key git-gutter+-mode-map (kbd "C-x r") 'git-gutter+-revert-hunks)
+     ;; Stage hunk at point.
+     ;; If region is active, stage all hunk lines within the region.
+     (define-key git-gutter+-mode-map (kbd "C-x t") 'git-gutter+-stage-hunks)
+     (define-key git-gutter+-mode-map (kbd "C-x c") 'git-gutter+-commit)
+     (define-key git-gutter+-mode-map (kbd "C-x C") 'git-gutter+-stage-and-commit)))
+
 ;; (global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
 ;; (global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
 
