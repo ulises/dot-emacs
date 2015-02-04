203a9b80863df1963921a2a29d512414f46c5d6a
Ulises <ulises.cervino@gmail.com>
2015-02-04 11:39:23 +0000

    Remove helm-find; add a bunch of haskell stuff

--- init.el
+++ init.el
@@ -234,11 +234,73 @@
 
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;; (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "M-i") 'helm-swoop)
 (global-set-key (kbd "C-c M-i") 'helm-multi-swoop-all)
 
 (load-theme 'leuven t)
+
+;; Haskell
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+(eval-after-load 'haskell-mode
+          '(define-key haskell-mode-map [f8] 'haskell-navigate-imports))
+
+(let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
+  (setenv "PATH" (concat my-cabal-path ":" (getenv "PATH")))
+  (add-to-list 'exec-path my-cabal-path))
+
+(custom-set-variables
+ '(haskell-process-type 'cabal-repl)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-tags-on-save t))
+(eval-after-load 'haskell-mode
+  '(progn
+     (define-key haskell-mode-map
+       (kbd "C-c C-l")
+       'haskell-process-load-or-reload)
+     (define-key haskell-mode-map
+       (kbd "C-c C-z")
+       'haskell-interactive-switch)
+     (define-key haskell-mode-map
+       (kbd "C-c C-n C-t")
+       'haskell-process-do-type)
+     (define-key haskell-mode-map
+       (kbd "C-c C-n C-i")
+       'haskell-process-do-info)
+     (define-key haskell-mode-map
+       (kbd "C-c C-n C-c")
+       'haskell-process-cabal-build)
+     (define-key haskell-mode-map
+       (kbd "C-c C-n c")
+       'haskell-process-cabal)
+     (define-key haskell-mode-map
+       (kbd "SPC")
+       'haskell-mode-contextual-space)
+     (define-key haskell-mode-map
+       (kbd "C-c C-o")
+       'haskell-compile)))
+(eval-after-load 'haskell-cabal
+  '(progn
+     (define-key haskell-cabal-mode-map
+       (kbd "C-c C-z")
+       'haskell-interactive-switch)
+     (define-key haskell-cabal-mode-map
+       (kbd "C-c C-k")
+       'haskell-interactive-mode-clear)
+     (define-key haskell-cabal-mode-map
+       (kbd "C-c C-c")
+       'haskell-process-cabal-build)
+     (define-key haskell-cabal-mode-map
+       (kbd "C-c c")
+       'haskell-process-cabal)
+     (define-key haskell-cabal-mode-map
+       (kbd "C-c C-o")
+       'haskell-compile)))
+
+
 
 ;; start the emacs server
 (server-start)
